e428c7a8977e0e13b0db8d65ca447ce0b5565ddb
KinnaGt <devilboogey@gmail.com>
2025-05-26 16:02:30 -0300

    add no quiero options functionality

--- Assets/Scripts/TrucoOptions.cs
+++ Assets/Scripts/TrucoOptions.cs
@@ -10,30 +10,110 @@ public class TrucoOptions : MonoBehaviour
     TypewritingSequences lucasText;
 
     [SerializeField]
+    TypewritingSequences polskyText;
+
+    [SerializeField]
+    TypewritingSequences offText;
+
+    [SerializeField]
     GameObject firstOptions;
 
     [SerializeField]
     Button trucoButton;
 
+    [SerializeField]
+    Button NoQuieroButton;
+
+    [SerializeField]
+    SpriteRenderer polskyImage;
+
+    [SerializeField]
+    SpriteRenderer lucasImage;
+
+    [SerializeField]
+    Animator sequenceAnimator;
+
+    [SerializeField]
     public void Truco()
     {
         lucasText.ChangeText("Pero... ¿Vos sos boludo Juan? No se puede cantar truco en un envido");
         firstOptions.SetActive(false);
+
         trucoButton.interactable = false;
         string newText =
             "<s>" + trucoButton.gameObject.GetComponent<TextMeshProUGUI>().text + "</s>";
         trucoButton.gameObject.GetComponent<TextMeshProUGUI>().text = newText;
+
         Destroy(trucoButton.GetComponent<ButtonHoverEffect>());
         Invoke("ShowEnvidoOptions", 5f);
     }
 
     void ShowEnvidoOptions()
     {
+        sequenceAnimator.enabled = true;
         lucasText.ChangeText("Cantá vos que ando flojo.");
         firstOptions.SetActive(true);
     }
 
-    public void Envido() { }
+    public void NoQuiero()
+    {
+        sequenceAnimator.enabled = false;
+        lucasText.ChangeText("");
+        polskyText.ChangeText("Sabíamos que no te daba el cuero, Juan.");
 
-    public void NoQuiero() { }
+        firstOptions.SetActive(false);
+        NoQuieroButton.interactable = false;
+        string newText =
+            "<s>" + NoQuieroButton.gameObject.GetComponent<TextMeshProUGUI>().text + "</s>";
+        NoQuieroButton.gameObject.GetComponent<TextMeshProUGUI>().text = newText;
+
+        Destroy(NoQuieroButton.GetComponent<ButtonHoverEffect>());
+        Invoke("ShowOffText", 3f);
+    }
+
+    public void ShowOffText()
+    {
+        polskyText.ChangeText("");
+        FadeImages();
+        offText.gameObject.SetActive(true);
+        Invoke("HideOffText", 5f);
+    }
+
+    public void HideOffText()
+    {
+        FadeImages(false);
+        offText.gameObject.SetActive(false);
+        lucasText.ChangeText("¿Qué te pasa Juan? ¿No querés jugar?");
+        polskyText.ChangeText("Envido! Vamos, no seas cagón!");
+        firstOptions.SetActive(true);
+    }
+
+    void FadeImages(bool fadeOut = true)
+    {
+        float targetAlpha = fadeOut ? 0f : 1f;
+        StartCoroutine(FadeImage(polskyImage, targetAlpha));
+        StartCoroutine(FadeImage(lucasImage, targetAlpha));
+    }
+
+    IEnumerator FadeImage(SpriteRenderer image, float targetAlpha)
+    {
+        Color color = image.color;
+        float startAlpha = color.a;
+        float elapsedTime = 0f;
+        float duration = .5f; // Duration of the fade effect
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / duration);
+            color.a = alpha;
+            image.color = color;
+            yield return null;
+        }
+
+        color.a = targetAlpha; // Ensure final alpha is set
+        image.color = color;
+    }
+
+    public void Envido() { }
 }
